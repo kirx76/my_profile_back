d153aba03673064fd79ecd6783340617f3be9c1a
kirx76 <kirx76>
2021-07-25 23:33:42 +0300

    allow patch request

--- src/app.ts
+++ src/app.ts
@@ -55,7 +55,7 @@ class App {
         res.setHeader('Access-Control-Allow-Origin', origin);
       }
       // res.header('Access-Control-Allow-Origin', 'http://192.168.193.243:3000');
-      res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+      res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE, PATCH");
       res.header('Access-Control-Allow-Credentials', 'true');
       res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
       next();
